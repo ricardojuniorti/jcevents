183bed6824eb9c9c770b2a3c22cbb793b2a12a2f
ricardojuniorti@gmail.com <ricardojuniorti@gmail.com>
2022-07-24 12:23:56 -0300

    segundo commit

--- resources/views/events/detailsEvent.blade.php
+++ resources/views/events/detailsEvent.blade.php
@@ -32,7 +32,7 @@
             </a>
           </form>
         @else
-          <p class="already-joined-msg">Você já está participando deste evento!</p>
+          <p class="already-joined-msg">Você já está participando deste evento.</p>
         @endif
         <h3>O evento conta com:</h3>
         <ul id="items-list">
